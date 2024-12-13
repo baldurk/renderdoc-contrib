aa21f1d880bf56dcbc7a2b2659711a395eebb65d
baldurk <baldurk@baldurk.org>
2024-12-13 13:55:19 +0000

    Add comments and README guiding from the empty extension to the docs

---
+++ example/empty-extension/README.md
@@ -0,0 +1,5 @@
+# Empty extension
+
+This extension is a completely empty example showing the bare bones of what is needed to register an extension with RenderDoc and get a callback to run custom code.
+
+For more information see [How do I register a python extension?](https://renderdoc.org/docs/how/how_python_extension.html) on the process of registering this extension with the UI, and [Writing UI extensions](https://renderdoc.org/docs/python_api/ui_extensions.html) for information on how to get started accessing the python API.
--- example/empty-extension/__init__.py
+++ example/empty-extension/__init__.py
@@ -1,5 +1,17 @@
 import qrenderdoc as qrd
 
+# This extension is a completely empty example showing the bare bones of what
+# is needed to register an extension with RenderDoc and get a callback to run
+# custom code.
+
+# For more information see:
+# https://renderdoc.org/docs/how/how_python_extension.html
+#
+# for the process of registering this extension with the UI, and:
+# https://renderdoc.org/docs/python_api/ui_extensions.html
+#
+# for information on how to get started accessing the python API.
+
 def register(version: str, ctx: qrd.CaptureContext):
     print("Registering empty extension")
 
