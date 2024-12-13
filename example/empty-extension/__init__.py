8f1dc38dfb433ab1c0730bbfd049d430efca05a7
baldurk <baldurk@baldurk.org>
2024-12-13 12:34:38 +0000

    Add an example of an empty UI extension

---
+++ example/empty-extension/__init__.py
@@ -0,0 +1,7 @@
+import qrenderdoc as qrd
+
+def register(version: str, ctx: qrd.CaptureContext):
+    print("Registering empty extension")
+
+def unregister():
+    print("Unregistering empty extension")
